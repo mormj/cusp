c9196d20a825ea2667ca9d8a0a30b5049b15b95c
siddarthmandayam <56770757+siddarthmandayam@users.noreply.github.com>
2021-06-03 13:20:09 -0400

    kernels: add_const
    * kernels/add_const.cu
    
    Signed-off-by: Sid Mandayam <siddarth.mandayam@peratonlabs.com>
    
    * kernerls/multiply_const.cu
    
    Signed-off-by: Sid Mandayam <siddarth.mandayam@peratonlabs.com>
    
    * qa_add_const file
    
    * added qa multiply file
    
    * added executables for qa_add and qa_multiply in meson.build file
    
    * added executables for qa_add and qa_multiply in meson.build
    
    * updated add const and meson build files
    
    Co-authored-by: Sid Mandayam <siddarth.mandayam@peratonlabs.com>
    Co-authored-by: mormj <34754695+mormj@users.noreply.github.com>

--- test/qa_copy.cu
+++ test/qa_copy.cu
@@ -9,6 +9,8 @@ TEST(CopyKernel, Basic)
 {
     int N = 1024*100;
     std::vector<std::complex<float>> input_data(N);
+    for (int i=0; i<N; i++)
+        input_data[i] = i;
     std::vector<std::complex<float>> output_data(N);
     launch_kernel_copy<uint64_t>((uint64_t *)input_data.data(), (uint64_t *)output_data.data(),
         1024, N / 1024, N);
