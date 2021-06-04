4068230366a477af5f44f8ac81c8e6bcb6962582
smandayam <siddarth.mandayam@gmail.com>
2021-06-04 16:06:41 -0400

    added multiply_const and fft_shift files

---
+++ include/cusp/fft_shift.cuh
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cusp/kernel.cuh>
+
+namespace cusp
+{
+
+template <typename T>
+class fft_shift : public kernel
+{
+public:
+    fft_shift() = default;
+    cudaError_t launch(const T *in, T *out, int grid_size, int block_size,
+        int N, cudaStream_t stream = 0);
+    virtual cudaError_t launch(const std::vector<const void *> inputs,
+        const std::vector<void *> outputs, size_t nitems) override;
+    virtual cudaError_t occupancy(int *minBlock, int *minGrid);
+};
+
+}
