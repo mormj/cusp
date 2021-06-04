e16ebb10d66c32043f0d8f725944293d85da7629
Josh Morman <jmorman@perspectalabs.com>
2021-06-04 13:09:04 -0400

    add kernel with multiple inputs
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

---
+++ include/cusp/add.cuh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cusp/kernel.cuh>
+
+namespace cusp
+{
+    template <typename T>
+    class add : public kernel
+    {
+    private:
+        T _ninputs;
+        void **_dev_ptr_array;
+    public:
+        add(T ninputs);
+        cudaError_t launch(const std::vector<const void *> inputs,
+            T* output, int ninputs, int grid_size, int block_size, size_t nitems, cudaStream_t stream = 0);
+        virtual cudaError_t launch(const std::vector<const void *> inputs,
+            const std::vector<void *> outputs, size_t nitems) override;
+        virtual cudaError_t occupancy(int *minBlock, int *minGrid);
+    };
+
+}
---
+++ test/qa_add.cu
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+#include <complex>
+#include <cusp/add.cuh>
+
+using namespace cusp;
+
+template <typename T> 
+void run_add_test(int N, T num_inputs)
+{
+    std::vector<T> host_input_data(N);
+    std::vector<T> expected_output_data(N);
+    for (int i = 0; i < N; i++) {
+      host_input_data[i] = (T)i;
+      expected_output_data[i] = num_inputs * i;
+    }
+    std::vector<T> host_output_data(N);
+  
+    void *dev_input_data;
+    void **dev_output_data;
+  
+    cudaMalloc(&dev_input_data, N * sizeof(T));
+    cudaMalloc(&dev_output_data, N * sizeof(T));
+  
+    std::cout << "ptr: " <<  dev_input_data << std::endl;
+
+    cudaMemcpy(dev_input_data, host_input_data.data(),
+               N * sizeof(T), cudaMemcpyHostToDevice);
+  
+    cusp::add<T> op(num_inputs);
+    int minGrid, minBlock;
+    op.occupancy(&minBlock, &minGrid);
+    op.set_block_and_grid(minGrid, N / minGrid);
+
+    std::vector<const void *> input_data_pointer_vec(num_inputs);
+    for (int i=0; i<num_inputs; i++)
+    {
+      input_data_pointer_vec[i] = dev_input_data;
+    }
+
+    op.launch(input_data_pointer_vec, {dev_output_data}, N);
+  
+    cudaDeviceSynchronize();
+    cudaMemcpy(host_output_data.data(), dev_output_data,
+               N * sizeof(T), cudaMemcpyDeviceToHost);
+  
+    EXPECT_EQ(expected_output_data, host_output_data);
+}
+
+
+TEST(Add, Basic) {
+  int N = 1024 * 100;
+
+  run_add_test<int16_t>(N, 3);
+  // run_add_test<float>(N, 4);
+}
