4b580d1e05ac2176678490cc5102c3b1e7f08f12
Mark <markb5@illinois.edu>
2021-06-07 11:25:09 -0400

    updated code for nlog10 and abs
    Signed-off-by: Mark <markb5@illinois.edu>

---
+++ kernels/nlog10.cu
@@ -0,0 +1,50 @@
+#include "../include/cusp/nlog10.cuh"
+#include <cuComplex.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <complex>
+
+namespace cusp {
+
+template <typename T>
+__global__ void kernel_nlog10(const T *in, T *out, T n, T k, int N) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < N) {
+    out[i] = (T)(n * log10(float(in[i])) + k);
+  }
+}
+
+template <typename T>
+cudaError_t nlog10<T>::launch(const T *in, T *out, T n, T k, int N, int grid_size,
+                              int block_size, cudaStream_t stream) {
+  if (stream) {
+    kernel_nlog10<<<grid_size, block_size, 0, stream>>>(in, out, n, k, N);
+  } else {
+    kernel_nlog10<<<grid_size, block_size>>>(in, out, n, k, N);
+  }
+  return cudaPeekAtLastError();
+}
+
+template <typename T>
+cudaError_t nlog10<T>::launch(const std::vector<const void *> inputs,
+                                 const std::vector<void *> outputs,
+                                 size_t nitems) {
+  return launch((const T *)inputs[0], (T *)outputs[0], _n, _k, nitems, _grid_size,
+                _block_size, _stream);
+}
+
+template <typename T>
+cudaError_t nlog10<T>::occupancy(int *minBlock, int *minGrid) {
+  return cudaOccupancyMaxPotentialBlockSize(minGrid, minBlock,
+                                            kernel_nlog10<T>, 0, 0);
+}
+
+#define IMPLEMENT_KERNEL(T) template class nlog10<T>;
+
+IMPLEMENT_KERNEL(int8_t)
+IMPLEMENT_KERNEL(int16_t)
+IMPLEMENT_KERNEL(int32_t)
+IMPLEMENT_KERNEL(int64_t)
+IMPLEMENT_KERNEL(float)
+
+} // namespace cusp
