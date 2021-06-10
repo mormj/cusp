d8a3b034793a7bf6989f3cd139b40ac0b2a83dd0
Mark <markb5@illinois.edu>
2021-06-10 14:53:42 -0400

    Added a bunch of kernels
    Signed-off-by: Mark <markb5@illinois.edu>

---
+++ include/cusp/absolute_value.cuh
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cusp/kernel.cuh>
+
+namespace cusp
+{
+
+template <typename T>
+class absolute_value : public kernel
+{
+public:
+    absolute_value() = default;
+    cudaError_t launch(const T *in, T *out, int grid_size, int block_size,
+        int N, cudaStream_t stream = 0);
+    virtual cudaError_t launch(const std::vector<const void *> inputs,
+        const std::vector<void *> outputs, size_t nitems) override;
+    virtual cudaError_t occupancy(int *minBlock, int *minGrid);
+};
+
+}
