bb93934334b905b78d559d0e902b60596e3abc40
smandayam <siddarth.mandayam@gmail.com>
2021-06-16 10:22:58 -0400

    add const

--- test/qa_add_const.cu
+++ test/qa_add_const.cu
@@ -26,10 +26,18 @@ void run_test(int N, T k)
                N * sizeof(T), cudaMemcpyHostToDevice);
   
     cusp::add_const<T> op(k);
+    /*
     int minGrid, minBlock;
     op.occupancy(&minBlock, &minGrid);
     op.set_block_and_grid(minGrid, N / minGrid);
     op.launch({dev_input_data}, {dev_output_data}, N);
+    */
+    int ncopies = N * sizeof(std::complex<float>) / sizeof(T);
+    int minGrid, blockSize, gridSize;
+    op.occupancy(&blockSize, &minGrid);
+    gridSize = (ncopies + blockSize - 1) / blockSize;
+    op.set_block_and_grid(blockSize, gridSize);
+    op.launch({dev_input_data}, {dev_output_data}, ncopies);
   
     cudaDeviceSynchronize();
     cudaMemcpy(host_output_data.data(), dev_output_data,
