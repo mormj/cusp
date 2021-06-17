c4a1aa6d224220db4fbdd6f3d94d89e675570add
Mark <markb5@illinois.edu>
2021-06-17 12:29:08 -0400

    fixed complex to mag kernels
    Signed-off-by: Mark <markb5@illinois.edu>

--- kernels/complex_to_mag_squared.cu
+++ kernels/complex_to_mag_squared.cu
@@ -1,16 +1,18 @@
 #include <cuComplex.h>
 #include <cuda.h>
+#include <complex>
+#include <thrust/complex.h>
 #include <cuda_runtime.h>
 #include "helper_cuda.h"
-#include <cusp/complex_to_mag_squared.cuh>
+#include "../include/cusp/complex_to_mag_squared.cuh"
 
 namespace cusp {
 
 template <typename T> __global__ void kernel_mag_squared(const T *in, T *out, int N) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   if (i < N) {
-    out[i].x = powf(in[i].x, 2) + powf(in[i].y, 2);
-    out[i].y = 0;
+    float mag_squared = in[i].real() * in[i].real() + in[i].imag() * in[i].imag();
+    out[i] = thrust::complex<float>(mag_squared, 0);
   }
 }
 
@@ -19,9 +21,11 @@ template <typename T>
 cudaError_t complex_to_mag_squared<T>::launch(const T *in, T *out, int N, int grid_size, int block_size,
                   cudaStream_t stream) {
   if (stream) {
-    kernel_mag_squared<<<grid_size, block_size, 0, stream>>>(in, out, N);
+    kernel_mag_squared<<<grid_size, block_size, 0, stream>>>((const thrust::complex<float> *)in, 
+                                                     (thrust::complex<float> *)out, N);
   } else {
-    kernel_mag_squared<<<grid_size, block_size>>>(in, out, N);
+    kernel_mag_squared<<<grid_size, block_size>>>((const thrust::complex<float> *)in, 
+                                          (thrust::complex<float> *)out, N);
   }
   return cudaPeekAtLastError();
 }
@@ -33,11 +37,13 @@ cudaError_t complex_to_mag_squared<T>::launch(const std::vector<const void *> in
 }
 
 template <typename T> cudaError_t complex_to_mag_squared<T>::occupancy(int *minBlock, int *minGrid) {
-  return cudaOccupancyMaxPotentialBlockSize(minGrid, minBlock, kernel_mag_squared<T>, 0, 0);
+  return cudaOccupancyMaxPotentialBlockSize(minGrid, minBlock,
+                                            kernel_mag_squared<thrust::complex<float>>,
+                                            0, 0);
 }
 
 #define IMPLEMENT_KERNEL(T) template class complex_to_mag_squared<T>;
 
-IMPLEMENT_KERNEL(cuFloatComplex)
+IMPLEMENT_KERNEL(std::complex<float>)
 
 } // namespace cusp
--- test/qa_complex_to_mag.cu
+++ test/qa_complex_to_mag.cu
@@ -1,48 +1,54 @@
 #include <gtest/gtest.h>
 #include <complex>
-#include <cusp/complex_to_mag.cuh>
+#include "../include/cusp/complex_to_mag.cuh"
 #include <cmath>
 #include <cuComplex.h>
 
 using namespace cusp;
 
-
 template <typename T> 
 void run_test(int N)
 {
-    std::vector<T> host_input_data(N);
-    std::vector<T> expected_output_data(N);
+    std::vector<std::complex<float>> host_input_data(N);
+    std::vector<std::complex<float>> expected_output_data(N);
     for (int i = 0; i < N; i++) {
-      host_input_data[i] = cuFloatComplex(float(i), float(i));
-      expected_output_data[i] = cuFloatComplex(sqrtf(2.0f) * float(i), 0);
+      host_input_data[i] = std::complex<float>(float(i), float(i * 2));
+      float mag = sqrtf(powf(host_input_data[i].real(), 2) + powf(host_input_data[i].imag(), 2));
+      expected_output_data[i] = std::complex<float>(mag, 0.0f);
     }
-    std::vector<T> host_output_data(N);
+    std::vector<std::complex<float>> host_output_data(N);
   
     void *dev_input_data;
     void *dev_output_data;
   
-    cudaMalloc(&dev_input_data, N * sizeof(T));
-    cudaMalloc(&dev_output_data, N * sizeof(T));
+    cudaMalloc(&dev_input_data, N * sizeof(std::complex<float>));
+    cudaMalloc(&dev_output_data, N * sizeof(std::complex<float>));
   
     cudaMemcpy(dev_input_data, host_input_data.data(),
-               N * sizeof(T), cudaMemcpyHostToDevice);
+               N * sizeof(std::complex<float>), cudaMemcpyHostToDevice);
   
-    cusp::complex_to_mag<T> op();
-    int minGrid, minBlock;
-    op.occupancy(&minBlock, &minGrid);
-    op.set_block_and_grid(minGrid, N / minGrid);
+    cusp::complex_to_mag<std::complex<float>> op;
+    int minGrid, blockSize, gridSize;
+    op.occupancy(&blockSize, &minGrid);
+    gridSize = (N + blockSize - 1) / blockSize;
+    op.set_block_and_grid(blockSize, gridSize);
     op.launch({dev_input_data}, {dev_output_data}, N);
   
     cudaDeviceSynchronize();
     cudaMemcpy(host_output_data.data(), dev_output_data,
-               N * sizeof(T), cudaMemcpyDeviceToHost);
+               N * sizeof(std::complex<float>), cudaMemcpyDeviceToHost);
   
-    EXPECT_EQ(expected_output_data, host_output_data);
+    //EXPECT_EQ(expected_output_data, host_output_data);
+    for (int i = 0; i < (int)expected_output_data.size(); i++) {
+      EXPECT_NEAR(expected_output_data[i].real(),
+                  host_output_data[i].real(),
+                  expected_output_data[i].real() / 10000);
+    }
 }
 
 
 TEST(ComplexToMagKernel, Basic) {
   int N = 1024 * 100;
 
-  run_test<cuFloatComplex>(N);
+  run_test<std::complex<float>>(N);
 }
