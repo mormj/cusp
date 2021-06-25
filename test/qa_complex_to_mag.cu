6cbad9bf6dbba7e689acf6b3d9b2692261ad9080
mormj <34754695+mormj@users.noreply.github.com>
2021-06-25 13:36:46 -0400

    cusp: remove templates from conj, complex_to_ (#26)
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

--- test/qa_complex_to_mag.cu
+++ test/qa_complex_to_mag.cu
@@ -5,28 +5,26 @@
 
 using namespace cusp;
 
-template <typename T> 
 void run_test(int N)
 {
     std::vector<std::complex<float>> host_input_data(N);
-    std::vector<std::complex<float>> expected_output_data(N);
+    std::vector<float> expected_output_data(N);
     for (int i = 0; i < N; i++) {
       host_input_data[i] = std::complex<float>(float(i), float(i * 2));
-      float mag = sqrtf(powf(host_input_data[i].real(), 2) + powf(host_input_data[i].imag(), 2));
-      expected_output_data[i] = std::complex<float>(mag, 0.0f);
+      expected_output_data[i] = sqrtf(powf(host_input_data[i].real(), 2) + powf(host_input_data[i].imag(), 2));
     }
-    std::vector<std::complex<float>> host_output_data(N);
+    std::vector<float> host_output_data(N);
   
     void *dev_input_data;
     void *dev_output_data;
   
     cudaMalloc(&dev_input_data, N * sizeof(std::complex<float>));
-    cudaMalloc(&dev_output_data, N * sizeof(std::complex<float>));
+    cudaMalloc(&dev_output_data, N * sizeof(float));
   
     cudaMemcpy(dev_input_data, host_input_data.data(),
                N * sizeof(std::complex<float>), cudaMemcpyHostToDevice);
   
-    cusp::complex_to_mag<std::complex<float>> op;
+    cusp::complex_to_mag op;
     int minGrid, blockSize, gridSize;
     op.occupancy(&blockSize, &minGrid);
     gridSize = (N + blockSize - 1) / blockSize;
@@ -35,13 +33,13 @@ void run_test(int N)
   
     cudaDeviceSynchronize();
     cudaMemcpy(host_output_data.data(), dev_output_data,
-               N * sizeof(std::complex<float>), cudaMemcpyDeviceToHost);
+               N * sizeof(float), cudaMemcpyDeviceToHost);
   
-    //EXPECT_EQ(expected_output_data, host_output_data);
-    for (int i = 0; i < (int)expected_output_data.size(); i++) {
-      EXPECT_NEAR(expected_output_data[i].real(),
-                  host_output_data[i].real(),
-                  expected_output_data[i].real() / 10000);
+    // EXPECT_EQ(expected_output_data, host_output_data);
+    for (size_t i = 0; i < expected_output_data.size(); i++) {
+      EXPECT_NEAR(expected_output_data[i],
+                  host_output_data[i],
+                  expected_output_data[i] / 10000);
     }
 }
 
@@ -49,5 +47,5 @@ void run_test(int N)
 TEST(ComplexToMagKernel, Basic) {
   int N = 1024 * 100;
 
-  run_test<std::complex<float>>(N);
+  run_test(N);
 }
